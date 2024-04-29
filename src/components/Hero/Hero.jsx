20a6803ceeefd1db7d67326184048c6fe01d38c2
badr <badrelhoudni96@gmail.com>
2024-04-29 16:48:19 +0100

    ..

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Hero.css'
-
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 const Hero = () => {
 	return (
 		<div className="hero">
@@ -15,17 +18,61 @@ const Hero = () => {
 			{/* Hero Heading part */}
 				<div className="hero-text">
 					<div>
-						<span>Shape </span>
+						<span className="stroke-text">Shape </span>
 						<span> Your </span>
 					</div>
 					<div>
 						<span>Ideal body </span>
 					</div>
+					<div>
+						<span>
+							In here we will help you to share and build your ideal body and live up your life to fullest
+						</span>
+					</div>
+				</div>
+				{/* figures part */}
+				<div className="figures">
+					<div>
+						<span>+140</span>
+						<span>Expert Coachs</span>
+					</div>
+					<div>
+						<span>+978</span>
+						<span>Members Joined</span>
+					</div>
+					 <div>
+                                                <span>+50</span>
+                                                <span>Fitness Programs</span>
+                                        </div>
+				</div>
+				{/* hero bouttons */}
+				<div className="hero-buttons">
+					<buttons className="btn">Get Started</buttons>
+					<buttons className="btn">Learn More</buttons>
 				</div>
 			</div>
-			<div cLassName="right-h">right side </div>
-		</div>
-	)
-}
+			<div cLassName="right-h">
+				<button className="btn">Join Now </button>
 
-export default Hero
+				<div className="heart-rate">
+					<img src={Heart} alt="" />
+					<span>Heart Rate </span>
+					<span>116 bpm </span>
+				</div>
+				{/* hero images */}
+				<img src={hero_image} alt="" className="hero-image" />
+				<img src={hero_image_back} alt="" className="hero-image-back" />
+				{/* calorie*/}
+				<div className="heart-rate">
+                                        <img src={Calories} alt="" />
+					<div>
+                                        	<span>Calories Burned </span>
+                                        	<span>230 Kcal</span>
+					</div>
+                                </div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
