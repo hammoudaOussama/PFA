dd09ea5adbd6823c355bc12aa35f9b52e0137d4f
badr <badrelhoudni96@gmail.com>
2024-05-08 18:21:36 +0100

    ..

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Plans.css';
-import {plansData} from '../data/plansData';
-import whiteTick from '../assets/whiteTick.png'
+import { plansData } from '../data/plansData';
+import whiteTick from '../assets/whiteTick.png';
 
-
-function Plan(props) {
+function Plans(props) {
     return (
         <div className='plans-container'>
             <div className="blur plans-blur-1"></div> 
@@ -17,23 +16,23 @@ function Plan(props) {
 
             {/* Plan Data */}
             <div className='plans'>
-                {plansData.map((plan,i) =>(
+                {plansData.map((plan, i) => (
                     <div className='plan' key={i}>
                         {plan.icon}
                         <span>{plan.name}</span>
                         <span>$ {plan.price}</span>
 
                         <div className='features'>
-                            {plan.features.map((feature,i)=>(
-                                <div className='feature'>
+                            {plan.features.map((feature, j) => (
+                                <div className='feature' key={j}>
                                     <img src={whiteTick} alt=""/>
-                                    <span key={i}>{feature}</span>
+                                    <span>{feature}</span>
                                 </div>
                             ))}
                         </div>
 
                         <div>
-                            <span  className='see'>see more benifits</span>
+                            <span  className='see'>see more benefits</span>
                         </div>
                         <button className='btn'>Join Now</button>
                     </div>
@@ -44,3 +43,4 @@ function Plan(props) {
 }
 
 export default Plans;
+
